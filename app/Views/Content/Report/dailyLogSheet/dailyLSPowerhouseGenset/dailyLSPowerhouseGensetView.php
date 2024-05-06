aac19090d6ae656b1c51775f53d3968e6708bb4e
ekoerwiswandoko <ekoerwiswandoko@gmail.com>
2024-05-06 14:53:54 +0700

    init report daily LS Powehouse Genset

---
+++ app/Views/Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/dailyLSPowerhouseGensetView.php
@@ -0,0 +1,335 @@
+<style type="text/css">
+	.datagrid-header .datagrid-cell{
+		line-height:normal;
+		/* height:auto; */
+	    white-space: normal;
+	}
+
+    .datagrid-cell-group{
+		line-height:normal;
+		height:auto;
+	    white-space: normal;
+	}
+</style>
+    
+    <!--- DATA GRID ------------------------------------------------------------------------->
+    <table id="dg" title="" class="easyui-datagrid"  style="<?php  echo $tinggi_dg ; ?>"  url="<?php echo site_url() . '/../Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/dataList'; ?>" toolbar="#tb-pv" data-options="striped:true,
+                    fitColumns:false,
+                    pagination:true,
+                    pageSize:50,
+                    showFooter: true,
+                    nowrap:false,
+                    singleSelect:true,
+                    rownumbers: true,
+                    frozenColumns:[[
+                        {field:'POSTDT',title:'TANGGAL',width:100,align:'center'},
+                        {field:'TIME_DISP',title:'JAM',width:60,align:'center'},
+                    ]],
+                    ">
+        <thead>
+            <tr>
+                <th rowspan="2" field="PWS_V" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>VOLTAGE</b></th>
+                <th colspan="3"><b>AMPERE</b></th>
+                <th rowspan="2" field="PWS_KWH1" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>KW</b></th>
+                <th rowspan="2" field="PWS_COST" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>COST</b></th>
+                <th rowspan="2" field="PWS_HZ" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>HZ</b></th>
+                <th rowspan="2" field="PWS_RPM" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>RPM</b></th>
+                <th rowspan="2" field="PWS_HM" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>HM</b></th>
+                <th rowspan="2" field="PWS_KWH2" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>KWH</b></th>
+                <th rowspan="2" field="PWS_TOIL" halign="center" data-options="sortable:false,width:100,align:'center' " formatter="formatNumberColumnCostum"><b>T OIL</b></th>
+                <th colspan="2"><b>TEMPERATURE</b></th>
+                <th rowspan="2" field="PWS_SLR_CM" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>SOLAR (CM)</b></th>
+                <!-- <th rowspan="3" field="TMP1" halign="center" data-options="sortable:false,width:60,align:'center' " ><b>PARAF MANDOR</b></th> -->
+                <!-- <th rowspan="3" field="TMP1" halign="center" data-options="sortable:false,width:160,align:'center' " ><b>KETERANGAN</b></th> -->
+            </tr>
+            <tr>
+                <th field="PWS_I_R" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>R</b></th>
+                <th field="PWS_I_S" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>S</b></th>
+                <th field="PWS_I_T" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>T</b></th>
+                <th field="PWS_MSN_TMP" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>MESIN</b></th>
+                <th field="PWS_OIL_TMP" halign="center" data-options="sortable:false,width:60,align:'center' " formatter="formatNumberColumnCostum"><b>OIL</b></th>
+            </tr>
+        </thead>
+    </table>
+
+    <div id="tb-pv" class="pb-1 pt-1">        
+        <div class='col-xl-12 col-lg-12 col-md-12 row'>
+            <div class="col row">
+                <input id="dt-tdate" name="TDATE" class="easyui-datebox" style="width: 150px;"  data-options="required:true"prompt="Start Date">
+                <input id="dt-tdate_end" name="TDATE_END" class="easyui-datebox" style="width: 150px;"  data-options="required:true" prompt="End Date">
+                <!-- <div class="col-xl-3 col-lg-3 col-md-3 row"> -->
+                    <!-- <input id="tb-Year" name="YEARNUMBER" class="easyui-numberbox " style="width: 100px;"  data-options="required:true" prompt="Year"> -->
+                    <!-- <input id="cg-MonthNumber" name="MONTHNUMBER" class="easyui-combogrid" style="width: 200px;"  data-options="required:true" prompt="Month"> -->
+                    
+                <!-- </div> -->
+            </div>
+            
+            <div class=" col-md-auto  text-right">
+                <button id="btn-search" class="btn btn-primary" style="width: 75px;"  onclick="doSearch()"><i class="fas fa-search"></i> Search</button>
+                &nbsp;
+                <button id="btn-searchReset" class="btn btn-danger" style="width: 75px;"  onclick="doSearchReset()"><i class="fas fa-eraser"></i> Clear</button>
+                <!-- &nbsp; -->
+                <!-- <button id="btn-print" class="btn btn-success" style="width: 75px;" onclick="exportDataExcel()" ><i class="fas fa-print"></i> .Xlsx</button> -->
+                &nbsp;
+                <button id="btn-print" class="btn btn-warning" style="width: 75px;" onclick="exportDataPDF()" ><i class="fas fa-print"></i> .Pdf</button>
+            </div>
+        </div>
+
+    </div>
+    <!-- </form> -->
+
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            settingCalendarTDATE();            
+            settingCalendarTDATE_END();            
+
+            // $('#cb-stationid').combobox({
+            //     valueField: 'ID',
+            //     textField: 'DESCRIPTION',
+            //     prompt:"Genset ID",
+            //     required:true,
+            //     value:"1",
+            //     url: "<?php // echo site_url() . '/../Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/getStationID'; ?>",
+            // });
+
+            doSearch();
+
+        });
+
+        function getMonthName(monthNumber) {
+            const date = new Date();
+            date.setMonth(monthNumber - 1);
+
+            return date.toLocaleString('en-US', { month: 'short' });
+        }
+
+
+        function settingCalendarTDATE(){
+
+            var d = new Date();
+            var enddate = d.setDate(d.getDate() - 1);
+
+            var paramDate = "<?php if(isset($_GET['POSTDT'])) { echo $_GET['POSTDT'];}  ?>";
+
+            console.log(paramDate);
+
+            if(paramDate != ''){    
+                var newD = new Date(paramDate);
+                enddate = newD;
+                
+            } else {
+                var newD = new Date(enddate);
+            }
+            
+            // var newD = new Date(enddate);
+            // alert(newD.getDate());
+
+            $('#dt-tdate').datebox({
+                value :  newD.getDate() + "-"+getMonthName(newD.getMonth()+1) + "-"+newD.getFullYear(),
+                // onSelect: function(date){
+                // var p_date = date.getDate()+"-"+(date.getMonth()+1) +"-"+date.getFullYear();
+                // }
+            });
+
+            $('#dt-tdate').datebox().datebox('calendar').calendar('moveTo', new Date(enddate));
+
+            // $('#dt-tdate').datebox().datebox('calendar').calendar({
+            //     validator: function(date){
+            //         var d = new Date();
+            //         var d2 = d.setDate(d.getDate() - 1);
+            //         return date <= d2;
+            //     }
+            // });
+        }
+
+        function settingCalendarTDATE_END(){
+
+            var d = new Date();
+            var enddate = d.setDate(d.getDate() - 1);
+
+            var paramDate = "<?php if(isset($_GET['POSTDT_END'])) { echo $_GET['POSTDT_END'];}  ?>";
+
+            // console.log(paramDate);
+
+            if(paramDate != ''){    
+                var newD = new Date(paramDate);
+                enddate = newD;
+                
+            } else {
+                var newD = new Date(enddate);
+            }
+
+            $('#dt-tdate_end').datebox({
+                value :  newD.getDate() + "-"+getMonthName(newD.getMonth()+1) + "-"+newD.getFullYear(),
+            });
+
+            $('#dt-tdate_end').datebox().datebox('calendar').calendar('moveTo', new Date(enddate));
+        }
+
+
+        // JS Searching and Reset
+        function doSearch() {
+
+            var dateParam = $('#dt-tdate').datebox('getValue');
+            var dateEndParam = $('#dt-tdate_end').datebox('getValue');
+            // var idParam =  $('#cb-stationid').combobox('getValue');
+
+            if( dateParam.trim() == '' || dateParam.trim() == null ){
+                // alert('"Tanggal" Harus Di Isi Dahulu');
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate').datebox('textbox').focus();
+                exit;   
+            }  
+
+            if( dateEndParam.trim() == '' || dateEndParam.trim() == null ){
+                // alert('"Tanggal" Harus Di Isi Dahulu');
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate_end').datebox('textbox').focus();
+                exit;   
+            } 
+
+            // if( idParam.trim() == '' || idParam.trim() == null ){
+            //     // alert('"Storage" Harus Di Isi Dahulu');
+            //     $.messager.alert({    
+            //         title: 'Info',
+            //         msg: 'Pilih Press ID Dahulu ! '
+            //     });
+            //     $('#cb-stationid').combobox('textbox').focus();
+            //     exit;   
+            // } 
+
+            $('#dg').datagrid('load', {
+                TDATE: $('#dt-tdate').datebox('getValue'),
+                TDATE_END: $('#dt-tdate_end').datebox('getValue'),
+                // STATIONID: $('#cb-stationid').combobox('getValue'),
+            });
+        }
+
+        function doSearchReset() {
+            $('#dt-tdate').datebox('reset');
+            $('#dt-tdate_end').datebox('reset');
+            // $('#cb-stationid').combobox('reset');
+
+        }
+
+        function formatNumberColumnCostum(val,row){
+            // return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            var returnVal ='';
+            if(val != null){
+                returnVal = parseFloat(val).format(2, 3, ',', '.');
+            } 
+            return  returnVal;
+        }
+
+        function formatNumberColumnCostumBilanganBulat(val,row){
+            // return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            var returnVal ='';
+            if(val != null){
+                returnVal = parseFloat(val).format(0, 3, ',', '.');
+            } 
+            return  returnVal;
+        }
+
+        function imageTrue(val,row){
+
+            var returnVal ='';
+            if(val > 0){
+                returnVal = '<i class="fas fa-check"></i>';
+            } 
+            return  returnVal;
+        }
+
+        function imageFalse(val,row){
+
+            var returnVal ='';
+            if(val < 1){
+                returnVal = '<i class="fas fa-times"></i>';
+            } 
+            return  returnVal;
+        }
+
+
+        function exportDataExcel() {
+        
+            var dateParam = $('#dt-tdate').datebox('getValue');
+            var dateEndParam = $('#dt-tdate_end').datebox('getValue');
+            // var idParam =  $('#cb-stationid').combobox('getValue');
+
+            if( dateParam.trim() == '' || dateParam.trim() == null ){
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate').datebox('textbox').focus();
+                exit;   
+            } 
+            if( dateEndParam.trim() == '' || dateEndParam.trim() == null ){
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate_end').datebox('textbox').focus();
+                exit;   
+            } 
+
+            // if( idParam.trim() == '' || idParam.trim() == null ){
+            //     $.messager.alert({    
+            //         title: 'Info',
+            //         msg: 'Pilih Press ID Dahulu ! '
+            //     });
+            //     $('#cb-stationid').combobox('textbox').focus();
+            //     exit;   
+            // } 
+
+            var url = "<?php  echo site_url() . '/../Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/exportExcelFile?TDATE='; ?>"+dateParam+"&TDATE_END="+dateEndParam;
+            // +"&STATIONID="+idParam;
+            window.open(url, "_blank");
+        }
+
+        function exportDataPDF() {
+        
+            var dateParam = $('#dt-tdate').datebox('getValue');
+            var dateEndParam = $('#dt-tdate_end').datebox('getValue');
+            // var idParam =  $('#cb-stationid').combobox('getValue');
+
+            if( dateParam.trim() == '' || dateParam.trim() == null ){
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate').datebox('textbox').focus();
+                exit;   
+            } 
+            if( dateEndParam.trim() == '' || dateEndParam.trim() == null ){
+                $.messager.alert({    
+                    title: 'Info',
+                    msg: 'Tanggal Harus Di Isi Dahulu ! '
+                });
+                $('#dt-tdate_end').datebox('textbox').focus();
+                exit;   
+            } 
+
+            // if( idParam.trim() == '' || idParam.trim() == null ){
+            //     $.messager.alert({    
+            //         title: 'Info',
+            //         msg: 'Pilih Press ID Dahulu ! '
+            //     });
+            //     $('#cb-stationid').combobox('textbox').focus();
+            //     exit;   
+            // } 
+
+             var url = "<?php  echo site_url() . '/../Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/exportPDFFile?TDATE='; ?>"+dateParam+"&TDATE_END="+dateEndParam;
+            //  +"&STATIONID="+idParam;
+            // var url = "<?php // echo site_url() . '/../Content/Report/dailyLogSheet/dailyLSPowerhouseGenset/cekPdfView?TDATE='; ?>"+dateParam+"&STATIONID="+idParam;
+            window.open(url, "_blank");
+    }
+
+    </script>   
